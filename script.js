2a5df8a6c261fdcf3c884c42e1cf6f80eda74349
Guaguag11 <dianagarzaguz11@gmail.com>
2023-04-14 21:10:56 -0600

    feat index

---
+++ script.js
@@ -0,0 +1,12 @@
+alert('hola!!!!')
+
+console.log("¡Hola, mundo!");
+
+console.log('No solo una palabra, sino una oración completa.');
+
+console.log("Java" + "Script");
+
+let numero =prompt('cuantos taquitos quieres');
+let taquitos= 'tu quieres' + ' ' +  numero  + ' ' + 'taquitos';
+
+console.log(taquitos);
