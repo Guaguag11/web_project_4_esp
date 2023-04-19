afcdf51807592da37bcb6bfd757cdde6b7fcfb7c
Guaguag11 <dianagarzaguz11@gmail.com>
2023-04-19 13:31:44 -0600

    fix sizes

--- form.js
+++ form.js
@@ -9,11 +9,13 @@ console.log(taquitos);
 
 */
 
+/* identificar variables */
+
 let editButton = document.querySelector(".profile__button-edit");
 let saveButton = document.querySelector(".form__button");
 let closeButton = document.querySelector(".form__button-close");
 
-let form = document.querySelector(".content__form");
+let form = document.querySelector(".modal__form");
 
 let pageName = document.querySelector(".profile__info-name");
 let profession = document.querySelector(".profile__info-tag");
@@ -23,17 +25,17 @@ let formTag = document.querySelector(".form__info-tag");
 /* eventos y adentro una función */
 
 editButton.addEventListener("click", function(){
-    form.classList.toggle("content__form-visible");
+    form.classList.toggle("modal__form-visible");
     formName.value = pageName.textContent;
     formTag.value = profession.textContent;
 });
 
 closeButton.addEventListener("click", function(){
-    form.classList.toggle("content__form-visible");
+    form.classList.toggle("modal__form-visible");
 });
 
 saveButton.addEventListener("click", function(){
-    form.classList.toggle("content__form-visible");
+    form.classList.toggle("modal__form-visible");
     pageName.textContent = formName.value;
     profession.textContent = formTag.value;
 })
