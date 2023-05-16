849c9fccce4dbf4cf00104e64294c20fe1ca4b84
Guaguag11 <dianagarzaguz11@gmail.com>
2023-05-15 20:08:43 -0600

    modal image

--- imagepop.js
+++ imagepop.js
@@ -3,24 +3,26 @@
 let openPic = document.querySelectorAll(".element__image");
 let closePic = document.querySelectorAll(".image__pop-close");
 
-let placeImage = document.querySelectorAll(".image");
+let modalImage = document.querySelectorAll(".modal__image");
 
 
 
-/*
-closePic.forEach(function(element, i){
+/*TENGO QUE HACER ESTO OTRA VEZ*/
+
+openPic.forEach(function(element, i){
     element.addEventListener("click", function () {
-    placeImage[i].classList.toggle("image__pop-up");
+    modalImage[i].classList.toggle("modal__image-visble");
     console.log("se hizo click");
 });
 });
-*/
 
-/*TENGO QUE HACER ESTO OTRA VEZ*/
 
 
 
 closePic.addEventListener("click", function(){
-    openPic.classList.toggle("image__pop-up");
-    console.log("se hizo click");
+    modalImage.classList.toggle("modal__image-visble");
+    console.log("se quiso cerrar");
 });
+
+
+/* en el close, no registra ni siquiera el click que hago */
