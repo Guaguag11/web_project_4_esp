d8679bd3ff7281f7ca58fda617bc8f4704db4d29
Guaguag11 <dianagarzaguz11@gmail.com>
2023-05-16 20:34:40 -0600

    fix doubts

--- formAdd.js
+++ formAdd.js
@@ -7,14 +7,15 @@ let formAdd = document.querySelector(".modal__form-add");
 
 let pageTitle = document.querySelector(".profile__info-title");
 let link = document.querySelector(".profile__info-url");
-let formTitle = document.querySelector(".element__name");
-let formUrl = document.querySelector(".SOS");
+let formTitle = document.querySelector(".element__name-modal");
+let formUrl = document.querySelector(".element__image-modal");
+
+
 
 /*SOS YA NO SE QUE HACER AQUI*/
 
 addButton.addEventListener("click", function(){
     formAdd.classList.toggle("modal__form-add-visible");
-
 });
 
 closeAddButton.addEventListener("click", function(){
@@ -23,5 +24,16 @@ closeAddButton.addEventListener("click", function(){
 
 saveAddButton.addEventListener("click", function(){
     formAdd.classList.toggle("modal__form-add-visible");
+    /* pageTitle.value = formTitle.textContent;
+    link.value = formUrl.textContent; */
+    console.log("se hizo click");
+
 })
 
+/*
+
+esto tengo que poner para que se agregue la url de los elements
+document.querySelector src
+
+Elements.openchild y el elemento nuevo 
+*/
--- imagepop.js
+++ imagepop.js
@@ -1,28 +1,27 @@
 
 
 let openPic = document.querySelectorAll(".element__image");
-let closePic = document.querySelectorAll(".image__pop-close");
+let closePic = document.querySelector(".image__pop-close");
 
 let modalImage = document.querySelectorAll(".modal__image");
 
 
 
-/*TENGO QUE HACER ESTO OTRA VEZ*/
-
 openPic.forEach(function(element, i){
     element.addEventListener("click", function () {
-    modalImage[i].classList.toggle("modal__image-visble");
+    modalImage[0].classList.toggle("modal__image-visible");
     console.log("se hizo click");
 });
 });
 
 
 
-
 closePic.addEventListener("click", function(){
-    modalImage.classList.toggle("modal__image-visble");
+    modalImage[0].classList.toggle("modal__image-visible");
     console.log("se quiso cerrar");
 });
 
 
-/* en el close, no registra ni siquiera el click que hago */
+
+/*
+que el image__pop el src sea igual al nuevo */
