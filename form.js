ef31f6dcf06a7dea09afa595cd59e58dd36987aa
Guaguag11 <dianagarzaguz11@gmail.com>
2023-05-22 14:25:25 -0600

    edit fix

--- form.js
+++ form.js
@@ -4,7 +4,7 @@ let editButton = document.querySelector(".profile__button-edit");
 let saveButton = document.querySelector(".form__button");
 let closeButton = document.querySelector(".form__button-close");
 
-let form = document.querySelector(".modal__form");
+let form = document.querySelector(".edit");
 
 let pageName = document.querySelector(".profile__info-name");
 let profession = document.querySelector(".profile__info-tag");
@@ -14,17 +14,17 @@ let formTag = document.querySelector(".form__info-tag");
 
 
 editButton.addEventListener("click", function(){
-    form.classList.toggle("modal__form-visible");
+    form.classList.toggle("edit-visible");
     formName.value = pageName.textContent;
     formTag.value = profession.textContent;
 });
 
 closeButton.addEventListener("click", function(){
-    form.classList.toggle("modal__form-visible");
+    form.classList.toggle("edit-visible");
 });
 
 saveButton.addEventListener("click", function(){
-    form.classList.toggle("modal__form-visible");
+    form.classList.toggle("edit-visible");
     pageName.textContent = formName.value;
     profession.textContent = formTag.value;
 })
