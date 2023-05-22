7fdad49dadc0b2e3e4841b2cdd79541d094452df
Guaguag11 <dianagarzaguz11@gmail.com>
2023-05-22 14:08:32 -0600

    fix image

--- imagepop.js
+++ imagepop.js
@@ -1,20 +1,19 @@
 let openPic = document.querySelectorAll(".element__image");
 let closePic = document.querySelector(".image__pop-close");
 
-let modalImage = document.querySelectorAll(".modal__image");
+let modalImage = document.querySelectorAll(".image");
 
 openPic.forEach(function (element, i) {
     console.log(element);
     element.addEventListener("click", function () {
-    modalImage[0].classList.toggle("modal__image-visible");
-    let img = modalImage[0].querySelector(".image__pop");
+    modalImage[0].classList.toggle("image__visible");
+    let img = modalImage[0].querySelector(".image__pop-up");
     img.src = element.src;
-    
     console.log("se hizo click");
     });
 });
 
 closePic.addEventListener("click", function () {
-    modalImage[0].classList.toggle("modal__image-visible");
+    modalImage[0].classList.toggle("image__visible");
     console.log("se cerro la ventana");
 });
