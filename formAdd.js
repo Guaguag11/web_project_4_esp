f6369ec8866aacf658978293def5dd9967920bee
Guaguag11 <dianagarzaguz11@gmail.com>
2023-05-22 14:19:39 -0600

    add fix

--- formAdd.js
+++ formAdd.js
@@ -2,7 +2,7 @@ let addButton = document.querySelector(".profile__button-add");
 let saveAddButton = document.querySelector(".form__button-add");
 let closeAddButton = document.querySelector(".form__button-add-close");
 
-let formAdd = document.querySelector(".modal__form-add");
+let formAdd = document.querySelector(".add");
 
 let title = document.querySelector(".form__info-title");
 let link = document.querySelector(".form__info-url");
@@ -14,17 +14,17 @@ let elements = document.querySelector(".elements");
 
 
 addButton.addEventListener("click", function () {
-    formAdd.classList.toggle("modal__form-add-visible");
+    formAdd.classList.toggle("add-visible");
 });
 
 closeAddButton.addEventListener("click", function () {
-    formAdd.classList.toggle("modal__form-add-visible");
+    formAdd.classList.toggle("add-visible");
 });
 
 saveAddButton.addEventListener("click", function () {
     let nodeClonado = node.cloneNode(true);
 
-    formAdd.classList.toggle("modal__form-add-visible");
+    formAdd.classList.toggle("add-visible");
 
     let formTitle = nodeClonado.querySelector(".element__name");
     let formLink = nodeClonado.querySelector(".element__image");
