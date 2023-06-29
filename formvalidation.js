2e6de0176425e14a1bf2310df37fe4d654caedb7
Guaguag11 <dianagarzaguz11@gmail.com>
2023-06-29 14:45:07 -0600

    1st input fix

--- formvalidation.js
+++ formvalidation.js
@@ -1,20 +1,20 @@
 const formularios = document.querySelectorAll(".form");
 const firstInputEdit = formularios[0].querySelector(".form__info-name");
-const firstSpanEdit = formularios[0].querySelector(".error__info-name");
+const firstSpanEdit = formularios[0].querySelector(".form__input-error-name");
 const secondInputEdit = formularios[0].querySelector(".form__info-tag");
-const secondSpanEdit = formularios[0].querySelector(".error__info-tag");
+const secondSpanEdit = formularios[0].querySelector(".form__input-error-tag");
 const buttonEdit = formularios[0].querySelector(".form__button");
 
 firstInputEdit.addEventListener("input", function(){
     if (!firstInputEdit.validity.valid) {
         buttonEdit.classList.add("form__button_inactive");
-        firstSpanEdit.classList.add("input__error-active");
+        firstSpanEdit.classList.add("form__input-error-active");
         firstSpanEdit.textContent = firstInputEdit.validationMessage;
         buttonEdit.disabled = true;
     }
     else{
         buttonEdit.classList.remove("form__button_inactive");
-        firstSpanEdit.classList.remove("input__error-active");
+        firstSpanEdit.classList.remove("form__input-error-active");
         firstSpanEdit.textContent = " ";
         buttonEdit.disabled = false;
     }
@@ -23,13 +23,13 @@ firstInputEdit.addEventListener("input", function(){
 secondInputEdit.addEventListener("input", function(){
     if (!secondInputEdit.validity.valid){
         buttonEdit.classList.add("form__button_inactive");
-        secondSpanEdit.classList.add("input__error-active");
+        secondSpanEdit.classList.add("form__input-error-active");
         secondSpanEdit.textContent = secondInputEdit.validationMessage;
         buttonEdit.disabled = true;
     }
     else{
         buttonEdit.classList.remove("form__button_inactive");
-        secondSpanEdit.classList.remove("input__error-active");
+        secondSpanEdit.classList.remove("form__input-error-active");
         secondSpanEdit.textContent = " ";
         buttonEdit.disabled = false;
     }
@@ -41,19 +41,17 @@ const secondInputAdd = formularios[1].querySelector(".form__info-url");
 const secondSpanAdd = formularios[1].querySelector(".error__info-link");
 const buttonAdd = formularios[1].querySelector(".form__button-add");
 
-console.log(secondInputAdd);
-console.log(secondSpanAdd);
 
 firstInputAdd.addEventListener("input",function(){
     if (!firstInputAdd.validity.valid){
         buttonAdd.classList.add("form__button_inactive");
-        firstSpanAdd.classList.add("input__error-active");
+        firstSpanAdd.classList.add("form__input-error-active");
         firstSpanAdd.textContent = firstInputAdd.validationMessage;
         buttonAdd.disabled = true;
     }
     else{
         buttonAdd.classList.remove("form__button_inactive");
-        firstSpanAdd.classList.remove("input__error-active");
+        firstSpanAdd.classList.remove("form__input-error-active");
         firstSpanAdd.textContent = " ";
         buttonAdd.disabled = false;
     }
@@ -63,12 +61,12 @@ firstInputAdd.addEventListener("input",function(){
 secondInputAdd.addEventListener("input",function(){
 if (!secondInputAdd.validity.valid){
     buttonAdd.classList.add("form__button_inactive");
-    secondSpanAdd.classList.add("input__error-active");
+    secondSpanAdd.classList.add("form__input-error-active");
     secondSpanAdd.textContent = secondInputAdd.validationMessage;
     buttonAdd.disabled = true;
 }else{
     buttonAdd.classList.remove("form__button_inactive");
-    secondSpanAdd.classList.remove("input__error-active");
+    secondSpanAdd.classList.remove("form__input-error-active");
     secondSpanAdd.textContent = " ";
     buttonAdd.disabled = false;
 }
